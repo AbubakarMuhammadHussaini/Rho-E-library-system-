e61cfc262f26cdaebff4c7832b6cb0727e637fa8
Abubakar Muhammad Husaaini <83621741+AbubakarMuhammadHussaini@users.noreply.github.com>
2021-07-19 01:44:47 -0700

    initial commit

---
+++ CHANGES/Books/Rho_E_library/Index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style type="text/css">
+<!--
+#apDiv1 {
+	position:absolute;
+	width:751px;
+	height:550px;
+	z-index:1;
+	left: 349px;
+	top: 1px;
+}
+#apDiv2 {
+	position:absolute;
+	width:566px;
+	height:62px;
+	z-index:2;
+	left: 173px;
+	top: 96px;
+	background-color: #00FF00;
+}
+.style1 {
+	font-size: xx-large;
+	font-weight: bold;
+}
+.style2 {
+	color: #FF0000
+}
+-->
+</style>
+</head>
+
+<body>
+<div id="apDiv1">
+  <table width="807" height="464" border="1">
+    <tr>
+      <td width="797" height="372"><img src="Books/getty_655998316_2000149920009280219_363765.jpg" width="810" height="365" />
+      <div class="style1" id="apDiv2">
+       <marquee behavior="slide"> <div align="center" class="style2">Rho_E_Library Access</div></marquee>
+      </div></td>
+    </tr>
+    <tr>
+      <td height="36"><div align="center"><img src="Books/Capture.PNG" width="542" height="70" /></div></td>
+    </tr>
+    <tr>
+      <td><div align="center"><img src="Books/Capture1.PNG" width="554" height="87" /></div></td>
+    </tr>
+  </table>
+</div>
+</body>
+</html>
---
+++ CHANGES/Books/Rho_E_library/Login.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style type="text/css">
+<!--
+#apDiv1 {
+	position:absolute;
+	width:397px;
+	height:176px;
+	z-index:2;
+	left: 501px;
+	top: 108px;
+}
+#apDiv2 {
+	position:absolute;
+	width:200px;
+	height:115px;
+	z-index:1;
+	left: 421px;
+	top: 75px;
+}
+.style5 {font-size: 24px; font-weight: bold; color: #FFFFFF; }
+-->
+</style>
+</head>
+
+<body>
+<div id="apDiv1">
+  <form id="form1" name="form1" method="post" action="">
+    <table width="380" height="116" border="1">
+      <tr>
+        <td width="171" height="40"><span class="style5">Username:</span></td>
+        <td width="193"><label>
+          <input name="Username" type="text" id="Username" size="30" maxlength="12" />
+        </label></td>
+      </tr>
+      <tr>
+        <td height="34"><span class="style5">Password:</span></td>
+        <td><input name="Password" type="text" id="Password" size="30" maxlength="12" /></td>
+      </tr>
+      <tr>
+        <td height="32"><label>
+          <div align="center">
+            <input type="reset" name="Reset" id="button" value="Reset" />
+            </div>
+        </label></td>
+        <td><div align="center">
+          <input type="submit" name="button2" id="button2" value="Login" />
+        </div></td>
+      </tr>
+    </table>
+  </form>
+</div>
+<div id="apDiv2"><img src="Books/How-to-Turn-off-Auto-Lock-in-Windows-10_4d470f76dc99e18ad75087b1b8410ea9.jpg" alt="" width="515" height="342" /></div>
+</body>
+
+</html>
